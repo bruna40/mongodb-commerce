30d5dc13174992dc98568a0337aaaeb777ac6c70
Bruna Santiago <brunasanthiago@outlook.com>
2022-12-27 14:27:32 -0300

    desafio13

---
+++ challenges/desafio13.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany({}, {
+    $set: {
+      criadoPor: "Ronald McDonald",
+    },
+  },
+  {
+    upsert: true,
+  });
+  
+  db.produtos.find({}, {
+    _id: 0,
+    nome: 1,
+    criadoPor: 1,
+  });
